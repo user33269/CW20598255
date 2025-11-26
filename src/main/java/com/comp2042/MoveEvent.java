33f542ef041ba5c9bc887a93e36807f7367a679d
qinee <qineefu@gmail.com>
2025-11-26 23:00:16 +0800

    -remove unused

--- src/main/java/com/comp2042/MoveEvent.java
+++ src/main/java/com/comp2042/MoveEvent.java
@@ -1,16 +1,11 @@
 package com.comp2042;
 
 public final class MoveEvent {
-    private final EventType eventType;
+
     private final EventSource eventSource;
 
-    public MoveEvent(EventType eventType, EventSource eventSource) {
-        this.eventType = eventType;
+    public MoveEvent( EventSource eventSource) {
         this.eventSource = eventSource;
-    }
-
-    public EventType getEventType() {
-        return eventType;
     }
 
     public EventSource getEventSource() {
