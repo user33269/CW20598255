04e781b2020746693fc29339ea11d7afb173cd48
qinee <qineefu@gmail.com>
2025-11-26 22:56:47 +0800

    -remove unused

--- src/main/java/com/comp2042/HomeController.java
+++ src/main/java/com/comp2042/HomeController.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -13,9 +12,6 @@ import javafx.stage.StageStyle;
 import java.io.IOException;
 
 public class HomeController {
-
-    @FXML
-    private Button zenButton;
 
     @FXML
     private void ZenMode(ActionEvent event){
